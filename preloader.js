fa55fb04d46c1b508e8a43e6f9901d18c41d01d8
D0NN4 <sgarbidonna@gmail.com>
2025-10-07 20:01:44 -0300

    observar scroll en mobile, ver preload

--- preloader.js
+++ preloader.js
@@ -2,5 +2,5 @@
   window.addEventListener('load', () => {
     setTimeout(() => {
       document.getElementById('preloader').classList.add('hide');
-    }, 4000); // 0.8s (expand) + 0.4s (fadein) + 0.8s (moveout)
+    }, 3000);
   });
